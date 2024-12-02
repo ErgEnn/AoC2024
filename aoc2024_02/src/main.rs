6f06de1fac44bbbedbc03bb29730ecdf6c9b18b5
Ergo Enn <ergo97@gmail.com>
2024-12-02 22:07:33 +0200

    Day 02

--- aoc2024_02/src/main.rs
+++ aoc2024_02/src/main.rs
@@ -1,21 +1,53 @@
-use util::{read_lines};
-
 fn main() {
-    let lines = read_lines("input.txt");
+    let reports = include_str!("../input.txt")
+        .lines()
+        .map(|line| line.split_whitespace().map(str::to_string).collect::<Vec<String>>())
+        .collect::<Vec<Vec<String>>>();
 
-    first_problem();
+    first_problem(&reports);
 
-    second_problem();
+    second_problem(&reports);
 }
 
-fn first_problem(){
-
-    println!("Answer 1: {}", 0);
+fn first_problem(reports: &Vec<Vec<String>>){
+    let result = reports.iter().filter(|report| is_report_valid(report)).count();
+    println!("Answer 1: {}", result);
 }
 
-fn second_problem(){
+fn is_report_valid(report: &Vec<String>)-> bool{
+    let mut report_iter = report.iter();
+    let mut prev = report_iter.next().unwrap().parse::<i32>().unwrap();
+    let mut dir = 0;
+    for reading_raw in report_iter {
+        let reading = reading_raw.parse::<i32>().unwrap();
+        if reading > prev && dir == -1 || reading < prev  && dir == 1{
+            return false;
+        }
+        let diff = reading-prev;
+        let diff_abs = diff.abs();
+        if diff_abs <= 3 && diff_abs >= 1{
+            dir = if reading > prev { 1 } else {-1};
+            prev = reading;
+            continue;
+        }
+        return false;
+    }
+    true
+}
 
-    println!("Answer 2: {}", 0);
+fn second_problem(reports: &Vec<Vec<String>>){
+    let result = reports
+        .iter()
+        .filter(|report| {
+        is_report_valid(report) ||
+            report
+                .iter()
+                .enumerate()
+                .map(|x| {[&report[..x.0], &report[x.0+1..]].concat()})
+                .any(|x| is_report_valid(&x))
+    })
+        .count();
+    println!("Answer 2: {}", result);
 }
 
 
