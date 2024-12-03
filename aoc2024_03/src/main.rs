6dd5e135f142c47bd3727e6c2812291700573d2a
Ergo Enn <ergo97@gmail.com>
2024-12-03 23:13:55 +0200

    Day 03

--- aoc2024_03/src/main.rs
+++ aoc2024_03/src/main.rs
@@ -1,20 +1,33 @@
+use regex::Regex;
 fn main() {
-    let elements = include_str!("../input.txt")
-        .lines()
-        .map(|line| line.split_whitespace().map(str::to_string).collect::<Vec<String>>())
-        .collect::<Vec<Vec<String>>>();
+    let input = include_str!("../input.txt");
 
-    first_problem(&elements);
+    first_problem(&input);
 
-    second_problem(&elements);
+    second_problem(&input);
 }
 
-fn first_problem(elements: &Vec<Vec<String>>){
-    let result = 0;
+fn first_problem(input: &str){
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let result = re.captures_iter(input).map(|cap| {
+        let a = cap.get(1).unwrap().as_str().parse::<i32>().unwrap();
+        let b = cap.get(2).unwrap().as_str().parse::<i32>().unwrap();
+        let c = a*b;
+        c
+    }).sum::<i32>();
     println!("Answer 1: {}", result);
 }
 
-fn second_problem(elements: &Vec<Vec<String>>){
-    let result = 0;
+fn second_problem(input: &str){
+    let re = Regex::new(r"(mul|don't|do)(?:\((?:|(\d+),(\d+))\))").unwrap();
+    let mut enabled = true;
+    let result = re.captures_iter(input).map(|cap| {
+        match cap.get(1).unwrap().as_str() {
+            "mul" => if enabled {cap.get(2).unwrap().as_str().parse::<i32>().unwrap() * cap.get(3).unwrap().as_str().parse::<i32>().unwrap()} else {0},
+            "don't" => {enabled = false; 0}
+            "do" => {enabled = true; 0}
+            _ => 0
+        }
+    }).sum::<i32>();
     println!("Answer 2: {}", result);
 }
